41d240cfbbbee9c2129a0e9442e9fe7a6b621a6f
Roger Vallès <roger.valles@socialpoint.es>
2014-04-08 14:36:21 +0200

    added wp_footer in home page

--- wp-content/themes/fga-photum/page-home.php
+++ wp-content/themes/fga-photum/page-home.php
@@ -64,4 +64,13 @@ get_header(); ?>
 	});
 	</script>
 
+	<?php
+	/* Always have wp_footer() just before the closing </body>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to reference JavaScript files.
+	 */
+
+	wp_footer();
+    ?>
+
 
