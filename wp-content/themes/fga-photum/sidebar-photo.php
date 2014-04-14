7d17b832ed791e7a039cbc0b859f9f31764b10f9
Roger Vallès <roger.valles@socialpoint.es>
2014-04-14 21:08:59 +0200

    some styling changes such as sidebar in single

--- wp-content/themes/fga-photum/sidebar-photo.php
+++ wp-content/themes/fga-photum/sidebar-photo.php
@@ -28,11 +28,32 @@
 				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
 				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-				<h1>meta goes here!!</h1>
 			</div><!-- #access -->
 
 
            		<div id="site-info">
+
+           		<div class="meta-info">	
+				<ul style="margin: 0 auto;"><li class="title"><?php the_title(); ?></li>
+                    <?php 
+                        $meta = get_post_meta( get_the_ID() ); 
+                        $exclude = array('_edit_last', '_wp_page_template', '_edit_lock','_thumbnail_id');
+                        foreach( $meta as $key => $value ) {
+                            if( in_array( $key, $exclude) )
+                                continue;
+                            ?>
+                            
+                                <li><?php echo $key; ?>: <?php echo $value[0]; ?></li>  
+                            
+                            <?php
+                        }
+                        ?>
+                </ul>
+                </div>
+
+                <br>
+
+
 				Copyright &copy; <?php echo date("Y"); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php bloginfo( 'name' ); ?>
 				</a>. All rights reserved.
