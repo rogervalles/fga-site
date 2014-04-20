c8d5a4794aa94a2aeda738358b56979b99035be4
Roger Vallès <roger.valles@socialpoint.es>
2014-04-20 21:02:48 +0200

    removed copyright + site info

--- wp-content/themes/fga-photum/sidebar-photo.php
+++ wp-content/themes/fga-photum/sidebar-photo.php
@@ -50,13 +50,6 @@
                         ?>
                 </ul>
                 </div>
-
-                <br>
-
-
-				Copyright &copy; <?php echo date("Y"); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
-				</a>. All rights reserved.
 			    </div><!-- #site-info -->
 	
 
--- wp-content/themes/fga-photum/sidebar.php
+++ wp-content/themes/fga-photum/sidebar.php
@@ -28,13 +28,11 @@
 				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
 				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+				
 			</div><!-- #access -->
 
 
            		<div id="site-info">
-				Copyright &copy; <?php echo date("Y"); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
-				</a>. All rights reserved.
 			    </div><!-- #site-info -->
 	
 
