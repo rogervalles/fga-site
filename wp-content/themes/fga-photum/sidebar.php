d63430c3599ccf497e20e715de3ef08445f568a2
Roger Vallès <roger.valles@socialpoint.es>
2014-04-26 18:17:10 +0200

    added meta info

--- wp-content/themes/fga-photum/sidebar.php
+++ wp-content/themes/fga-photum/sidebar.php
@@ -28,6 +28,14 @@
 				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
 				<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
 				<?php wp_nav_menu( array( 'container' => 'nav', 'container_class' => 'menu-header', 'theme_location' => 'primary', 'depth' => 	-1 ) ); ?>
+
+				<div class="meta-info">	
+				<ul><!--li class="title"><?php /*the_title();*/ ?></li>-->
+                    <?php 
+                        echo get_the_excerpt();
+                        ?>
+                </ul>
+                </div>
 				
 			</div><!-- #access -->
 
