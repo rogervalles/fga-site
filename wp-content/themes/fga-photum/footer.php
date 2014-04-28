5a0af7cbe4fa5aa3882c50263be133a19331e943
Roger Vallès <roger.valles@socialpoint.es>
2014-04-29 00:37:17 +0200

    set column width for masonry

--- wp-content/themes/fga-photum/footer.php
+++ wp-content/themes/fga-photum/footer.php
@@ -12,20 +12,23 @@
 ?>
 	</div><!-- #main -->
     </div><!-- #wrapper -->
+  </div> <!--toogle nav-->
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.masonry.min.js"></script>
+
 <script>
   $(function(){
     
     console.time('masonry');
     $('#move').masonry({
       itemSelector: '.item',
-      columnWidth: 4,
+      columnWidth: 260,
       isAnimated: true
     }).masonry().masonry();
     console.timeEnd('masonry');
     
   });
 </script>
+
 <?php
 	/* Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
