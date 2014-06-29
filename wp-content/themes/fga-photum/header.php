73fdd766cdba175dcfc33881f360cc182b1981be
Roger Vallès <roger.valles@socialpoint.es>
2014-06-29 18:30:19 +0200

    modify viewport

--- wp-content/themes/fga-photum/header.php
+++ wp-content/themes/fga-photum/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title><?php
 	global $page, $paged;
 	wp_title( '|', true, 'right' );
@@ -15,8 +15,8 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/pagenavi-css.css" type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
 <?php
 	add_action('wp_enqueue_scripts', 'loading_jquery');
 	if ( is_singular() && get_option( 'thread_comments' ) )
@@ -26,41 +26,6 @@
     wp_enqueue_script( 'jquery' );
 	wp_head();
 ?>
-<script>
-
-$(document).ready(function() {
-
-	//move the image in pixel
-	var move = 0;
-	
-	//zoom percentage, 1.2 =120%
-	var zoom = 2;
-
-	//On mouse over those thumbnail
-	$('.item').hover(function() {
-		
-		//Set the width and height according to the zoom percentage
-		width = $('.item').width() * zoom;
-		height = $('.item').height() * zoom;
-		
-		//Move and zoom the image
-		$(this).find('img').stop(false,true).animate({'width':width, 'height':height, 'top':move, 'left':move}, {duration:200});
-		
-		//Display the caption
-		$(this).find('div.caption').stop(false,true).fadeIn(200);
-	},
-	function() {
-		//Reset the image
-		$(this).find('img').stop(false,true).animate({'width':$('.item').width(), 'height':$('.item').height(), 'top':'0', 'left':'0'}, {duration:100});	
-
-		//Hide the caption
-		$(this).find('div.caption').stop(false,true).fadeOut(200);
-	});
-
-});
-
-</script>
-
 
 </head>
 
@@ -76,3 +41,5 @@ $(document).ready(function() {
   ?>
   	<div id="main"> 
     <div id="home-wrap">
+
+      
